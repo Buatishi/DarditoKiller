1407997a738c69c2be98f1c79d7d09f15ae25529
Bautishi <bautistagiraud@gmail.com>
2025-09-30 10:21:56 -0300

    Primer commit

---
+++ src/scenes/menus.js
@@ -0,0 +1,63 @@
+class MainMenu extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'MainMenu'
+        });
+    }
+
+    preload(){
+
+    }
+
+    create(){
+
+    }
+}
+
+class PauseMenu extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'PauseMenu'
+        });
+    }
+
+    preload(){
+
+    }
+
+    create(){
+
+    }
+}
+
+class GameOnMenu extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'GameOnMenu'
+        });
+    }
+
+    preload(){
+
+    }
+
+    create(){
+
+    }
+}
+
+class GameOverMenu extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'GameOverMenu'
+        });
+    }
+
+    preload(){
+        
+    }
+
+    create(){
+
+    }
+}
